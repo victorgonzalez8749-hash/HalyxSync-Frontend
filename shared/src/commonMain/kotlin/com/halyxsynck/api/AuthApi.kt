769c0658926026db712824a7e0775f6757b85214
Victor <victorgonzalez8749@gmail.com>
2026-07-08 13:33:12 -0600

    ACTUALIZACION DE FRONTEND2.0

--- shared/src/commonMain/kotlin/com/halyxsynck/api/AuthApi.kt
+++ shared/src/commonMain/kotlin/com/halyxsynck/api/AuthApi.kt
@@ -1,17 +1,17 @@
 package com.halyxsynck.api
 
+import com.halyxsynck.model.LoginRequest
+import com.halyxsynck.model.LoginResponse
 import com.halyxsynck.model.RegisterRequest
 import com.halyxsynck.model.RegisterResponse
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
-import com.halyxsynck.model.LoginRequest
-import com.halyxsynck.model.LoginResponse
 
-import io.ktor.client.plugins.*
-import io.ktor.client.statement.*
 class AuthApi {
 
     private val baseUrl =
@@ -33,6 +33,8 @@ class AuthApi {
 
         } catch (e: ClientRequestException) {
 
+            println("CLIENT ERROR: ${e.response.status}")
+
             RegisterResponse(
                 success = false,
                 mensaje = "Solicitud inválida"
@@ -40,9 +42,20 @@ class AuthApi {
 
         } catch (e: ServerResponseException) {
 
+            println("SERVER ERROR: ${e.response.status}")
+
             RegisterResponse(
                 success = false,
                 mensaje = "Error interno del servidor"
+            )
+
+        } catch (e: Exception) {
+
+            e.printStackTrace()
+
+            RegisterResponse(
+                success = false,
+                mensaje = "No se pudo conectar con el servidor"
             )
 
         }
@@ -53,13 +66,26 @@ class AuthApi {
         data: LoginRequest
     ): LoginResponse {
 
-        return client.post("$baseUrl/auth/login") {
+        return try {
 
-            contentType(ContentType.Application.Json)
+            client.post("$baseUrl/auth/login") {
 
-            setBody(data)
+                contentType(ContentType.Application.Json)
 
-        }.body()
+                setBody(data)
+
+            }.body()
+
+        } catch (e: Exception) {
+
+            e.printStackTrace()
+
+            LoginResponse(
+                success = false,
+                mensaje = "Error al iniciar sesión"
+            )
+
+        }
 
     }
 
